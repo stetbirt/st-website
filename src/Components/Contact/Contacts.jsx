b77ce7d1aea8c88583ebe9c9c14db631646bf5d4
KhaledAT <tetbirtk@gmail.com>
2025-03-11 16:30:50 -0400

    display image fix

--- src/Components/Contact/Contacts.jsx
+++ src/Components/Contact/Contacts.jsx
@@ -18,7 +18,7 @@ const Contacts = () => {
                                                     <path
                                                         d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/>
                                                 </svg>
-                                                <h5 className="text-black text-base font-normal leading-6 ml-5">stetbirt@gmail.com</h5>
+                                                <h5 className="text-black text-base font-normal leading-6 ml-5">Coming Soon</h5>
                                             </a>
                                             <a href="" className="link-item flex items-center mb-6">
                                                 <span className="[&>svg]:h-5 [&>svg]:w-5">
